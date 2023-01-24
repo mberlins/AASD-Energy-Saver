b666a964d423167185e5c1816f3bdeaeebde1253
Łukasz Błocki <44443287+LBlocki@users.noreply.github.com>
2023-01-24 21:20:33 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
 sudo prosodyctl register sensors localhost password
+sudo prosodyctl register blinds localhost password
+sudo prosodyctl register heating localhost password
+sudo prosodyctl register lighting localhost password
+sudo prosodyctl register windows localhost password
